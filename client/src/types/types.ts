c776a85ba4d7b5977e8e80cb1990f17686c2db2d
corey clarke <sombas100@yahoo.com>
2024-09-03 21:27:07 +0100

    Setup the frontend

---
+++ client/src/types/types.ts
@@ -0,0 +1,13 @@
+export interface Project {
+    id: number;
+    name: string;
+    description: string;
+}
+
+
+export interface Task {
+    id: number;
+    title: string;
+    description: string;
+    status: string;
+}

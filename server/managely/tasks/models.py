3a2f42d4757461f2de9f0efb8dc96955d48764b5
corey clarke <sombas100@yahoo.com>
2024-09-03 19:35:07 +0100

    first commit

---
+++ server/managely/tasks/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('TODO', 'To Do'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed'),
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default='TODO')
+    project = models.ForeignKey(
+        Project, related_name='tasks', on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(
+        User, related_name='tasks', on_delete=models.SET_NULL, blank=True, null=True)
